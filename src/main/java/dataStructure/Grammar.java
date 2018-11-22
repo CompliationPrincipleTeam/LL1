7b1366c8fde2646840e47d4068dff012fdfcc949
xuefanggang <xuefanggang97@gmail.com>
2018-11-22 15:06:29 +0800

    左递归消除

--- src/main/java/dataStructure/Grammar.java
+++ src/main/java/dataStructure/Grammar.java
@@ -4,34 +4,46 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class Grammar {
-	public String S="";							//开始符号也可以是：production1[0].left
-	public ArrayList<Production> production2;	//p2是把 E->A|b分开两个的产生式集合
-	public ArrayList<Production> production1;	//p1是把 E->A|b当作一个产生式
-	public HashSet<String> vt;	//终结符集合
-	public HashSet<String> vn;	//非终结符集合
-	
-	public Grammar()
-	{
-		production1 = new ArrayList<Production>();
-		production2 = new ArrayList<Production>();
-		vt = new HashSet<String>();
-		vn = new HashSet<String>();
-	}
-	
-	
-	public void addProduction(String left, String right)
-	{
-		this.production1.add(new Production(left,right));
-		String[] r = right.split("\\|");
-		for(int i=0;i<r.length;i++)
-		{
-			Production p = new Production();
-			p.leftPart = left;
-			p.rightPart = r[i];
-			this.production2.add(p);
-			System.out.println(p.leftPart +"->"+p.rightPart);
-		}
-	}
-	
+    public String S = "";                            //开始符号也可以是：production1[0].left
+    public ArrayList<Production> production2;    //p2是把 E->A|b分开两个的产生式集合
+    public ArrayList<Production> production1;    //p1是把 E->A|b当作一个产生式
+    public HashSet<String> vt;    //终结符集合
+    public HashSet<String> vn;    //非终结符集合
+    public ArrayList<String> strGrammer;
+
+    public Grammar() {
+        production1 = new ArrayList<Production>();
+        production2 = new ArrayList<Production>();
+        vt = new HashSet<String>();
+        vn = new HashSet<String>();
+        strGrammer = new ArrayList<String>();
+
+    }
+
+    public void clearGrammer(){
+        production1.clear();
+        production2.clear();
+
+    }
+
+    public void addProduction(String left, String right, boolean addTostr) {
+
+        if (addTostr) {
+            strGrammer.add(left + "->" + right);
+        }
+
+        this.production1.add(new Production(left, right));
+        String[] r = right.split("\\|");
+        for (int i = 0; i < r.length; i++) {
+            Production p = new Production();
+            p.leftPart = left;
+            p.rightPart = r[i];
+            this.production2.add(p);
+          //  System.out.println(p.leftPart + "->" + p.rightPart);
+        }
+
+       // System.out.println("add end");
+    }
+
 
 }
